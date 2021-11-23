fd020c8082d489b5c1e2abb4499d3d6ea91ac748
Musa Çam <musacamm@gmail.com>
2021-11-24 01:07:27 +0300

    Linkedin Clone Setup + Firebase

---
+++ linkedin/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCa4yL7_NmFNg6g5EONYuYfejbCON0OPTk",
+    authDomain: "linkedin-clone-1676f.firebaseapp.com",
+    projectId: "linkedin-clone-1676f",
+    storageBucket: "linkedin-clone-1676f.appspot.com",
+    messagingSenderId: "541092680059",
+    appId: "1:541092680059:web:75e3b7b661855b56f16dde"
+};
