79c903925844faa46ebffd5ab355239f12f3ab4b
Musa Çam <musacamm@gmail.com>
2021-12-01 00:06:19 +0300

    Redux Implementation

--- linkedin/src/App.js
+++ linkedin/src/App.js
@@ -3,15 +3,23 @@ import './css/App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
+import Login from './Login';
+import {useSelector} from 'react-redux';
+import { selectUser } from './features/userSlice';
 
 function App() {
+
+  const user = useSelector(selectUser)
+
   return (
     <div className="App">
       <Header/>
-      <div className="AppBody">
-        <Sidebar/>
-        <Feed/>
-      </div>
+      {!user ? (<Login/>) : (
+        <div className="AppBody">
+          <Sidebar/>
+          <Feed/>
+        </div>
+      )}
     </div>
   );
 }
---
+++ linkedin/src/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./css/Login.css"
+
+function Login() {
+    return (
+        <div className="Login">
+            You are not logged in.
+        </div>
+    )
+}
+
+export default Login
