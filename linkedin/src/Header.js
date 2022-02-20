0bba14369d41179ca4b5bf5c066710031fcd2249
Musa Çam <muhammetmusa.cam@bahcesehir.edu.tr>
2022-02-20 19:42:25 +0300

    Update

--- linkedin/src/Header.js
+++ linkedin/src/Header.js
@@ -1,46 +1,48 @@
-import React from 'react'
-import "./css/Header.css"
-import SearchIcon from '@material-ui/icons/Search';
-import HeaderOption from './HeaderOption';
-import HomeIcon from '@material-ui/icons/Home';
-import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
-import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
-import ChatIcon from '@material-ui/icons/Chat';
-import NotificationIcon from '@material-ui/icons/Notifications';
-import PersonIcon from '@material-ui/icons/Person';
-import { useDispatch } from 'react-redux';
-import { auth } from './firebase';
-import { logout } from './features/userSlice';
+import React from "react";
+import "./css/Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
+import ChatIcon from "@material-ui/icons/Chat";
+import NotificationIcon from "@material-ui/icons/Notifications";
+import PersonIcon from "@material-ui/icons/Person";
+import { useDispatch } from "react-redux";
+import { auth } from "./firebase";
+import { logout } from "./features/userSlice";
 
 function Header() {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const logoutOfApp = () => {
+    dispatch(logout());
+    auth.signOut();
+  };
 
-    const logoutOfApp=()=>{
-        dispatch(logout());
-        auth.signOut();
-    }
-
-    return (
-        <div className="Header">
-            <div className="HeaderLeft">
-            <img src="https://img.icons8.com/fluency/48/000000/linkedin.png" alt=""/>
-                <div className="HeaderSearch">
-                    <SearchIcon/>
-                    <input type="text" placeholder="Search"/>
-                </div>
-            </div>
-
-            <div className="HeaderRight">
-                <HeaderOption Icon={HomeIcon} title="Home"/>
-                <HeaderOption Icon={SupervisorAccountIcon} title="My Network"/>
-                <HeaderOption Icon={BusinessCenterIcon} title="Jobs"/>
-                <HeaderOption Icon={ChatIcon} title="Messaging"/>
-                <HeaderOption Icon={NotificationIcon} title="Notifications"/>
-                <HeaderOption Icon={PersonIcon} title="Me" onClick={logoutOfApp}/>
-            </div>
+  return (
+    <div className="Header">
+      <div className="HeaderLeft">
+        <img
+          src="https://img.icons8.com/fluency/48/000000/linkedin.png"
+          alt=""
+        />
+        <div className="HeaderSearch">
+          <SearchIcon />
+          <input type="text" placeholder="Search" />
         </div>
-    )
+      </div>
+
+      <div className="HeaderRight">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationIcon} title="Notifications" />
+        <HeaderOption Icon={PersonIcon} title="Me" onClick={logoutOfApp} />
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
